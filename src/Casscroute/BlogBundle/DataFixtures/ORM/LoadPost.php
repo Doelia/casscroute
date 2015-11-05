3119a77eadab2b453489995d4beab1c004bd8a26
cavalet.pierre@gmail.com <pierre.cavalet@etu.umontpellier.fr>
2015-11-05 17:15:12 +0100

    Ajout post fixtures

---
+++ src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Casscroute\BlogBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Casscroute\BlogBundle\Entity\Post;
+
+class LoadPost implements FixtureInterface
+{
+  public function load(ObjectManager $manager)
+  {
+    for($i = 0; $i < 15; $i++)
+    {
+      $post = new Post();
+      $post->setTitle("titre " . $i);
+      $post->setUrlAlias("alias " . $i);
+      $post->setContent("contenu " . $i);
+      $post->setPublished(new \DateTime("2015-01-01 10:00:" . $i));
+      $manager->persist($post);
+    }
+
+    $manager->flush();
+  }
+}
