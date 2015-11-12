fff7f7a7b590cb6a2d4f64116f37f5ec2854f64f
Doelia <kyxalia@gmail.com>
2015-11-12 13:56:31 +0100

    Cleanup code

--- src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
+++ src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Casscroute\BlogBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
@@ -9,16 +8,17 @@ use Casscroute\BlogBundle\Entity\Post;
 
 class LoadPost implements FixtureInterface
 {
+
   public function load(ObjectManager $manager)
   {
-    for($i = 0; $i < 15; $i++)
+    for ($i = 0; $i < 15; $i++)
     {
-      $post = new Post();
-      $post->setTitle("titre " . $i);
-      $post->setUrlAlias("alias " . $i);
-      $post->setContent("contenu " . $i);
-      $post->setPublished(new \DateTime("2015-01-01 10:00:" . $i));
-      $manager->persist($post);
+        $post = new Post();
+        $post->setTitle("titre " . $i);
+        $post->setUrlAlias("alias " . $i);
+        $post->setContent("contenu " . $i);
+        $post->setPublished(new \DateTime("2015-01-01 10:00:" . $i));
+        $manager->persist($post);
     }
 
     $manager->flush();
