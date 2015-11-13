a246eb36973fe6ddd352037af823fe55636ca96e
Doelia <kyxalia@gmail.com>
2015-11-13 09:54:04 +0100

    Cleanup

--- src/Casscroute/BlogBundle/Controller/CrudController.php
+++ src/Casscroute/BlogBundle/Controller/CrudController.php
@@ -13,13 +13,16 @@ class CrudController extends Controller
     {
         $post = new Post();
         $form = $this->get('form.factory')->create(new PostType(), $post);
-        if ($form->handleRequest($request)->isValid()){
-          $em = $this->getDoctrine()->getManager();
-          $em->persist($post);
-          $em->flush();
 
-          $request-> getSession()->getFlashBag()->add('notice', 'Post bien enregistré.');
+        if ($form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            $request-> getSession()->getFlashBag()->add('notice', 'Post bien enregistré.');
         }
+
         return $this->render('CasscrouteBlogBundle:Crud:new.html.twig', array('form' => $form->createView()));
     }
 
@@ -29,19 +32,21 @@ class CrudController extends Controller
         $postRepository = $em->getRepository('CasscrouteBlogBundle:Post');
         $post = $postRepository->find($id);
         $form = $this->get('form.factory')->create(new PostType(), $post);
-        if ($form->handleRequest($request)->isValid()){
-          $em = $this->getDoctrine()->getManager();
-          $em->persist($post);
-          $em->flush();
 
-          $request-> getSession()->getFlashBag()->add('notice', 'Post bien modifié.');
+        if ($form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            $request-> getSession()->getFlashBag()->add('notice', 'Post bien modifié.');
         }
+
         return $this->render('CasscrouteBlogBundle:Crud:edit.html.twig', array('form' => $form->createView()));
     }
 
     public function deleteAction($id)
     {
-        
         return $this->render('CasscrouteBlogBundle:Blog:post.html.twig', array('id' => $id));
     }
 }
