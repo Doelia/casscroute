3994ee2b5b36a90fc80ef83c7084716dc8b1e6bf
Doelia <kyxalia@gmail.com>
2015-12-03 20:13:55 +0100

    AJout datafixture

--- src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
+++ src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
@@ -11,12 +11,12 @@ class LoadPost implements FixtureInterface
 
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i < 15; $i++)
+        for ($i = 1; $i < 15; $i++)
         {
             $post = new Post();
-            $post->setTitle("titre " . $i);
+            $post->setTitle("Article " . $i);
             $post->setUrlAliasSlugified($post->getTitle());
-            $post->setContent("contenu " . $i);
+            $post->setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta velit faucibus, laoreet nibh sit amet, molestie dui. Nulla eu ante lectus. Ut tempus, felis a accumsan mattis, lectus mi lacinia diam, malesuada dictum mi ipsum et mi. Nulla cursus sem tellus, eu tincidunt mauris ornare eu. Mauris ut dui viverra mauris ultrices lacinia. Mauris non ante eleifend, scelerisque nulla vitae, aliquet ligula. Proin dui risus, tristique at tortor ultrices, maximus suscipit magna. Nullam felis magna, laoreet ac mattis sed, feugiat eu mi. Donec ut consectetur sapien, a interdum ipsum. Duis consequat enim non tellus laoreet ornare. Nullam et tristique risus, iaculis semper nibh.");
             $post->setPublished(new \DateTime("2015-01-01 10:00:" . $i));
             $manager->persist($post);
         }
