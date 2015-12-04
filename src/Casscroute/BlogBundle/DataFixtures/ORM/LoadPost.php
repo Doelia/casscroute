c71bc2e84e3807694abbda54a88e790549871058
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-12-04 11:32:24 +0100

    Modif datafixtures pour les posts

--- src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
+++ src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
@@ -25,21 +25,57 @@ class LoadPost implements FixtureInterface
 - 8 fines tranches de concombre
 - 4 tranches de fromage
 - 1 avocat *bien mûr*
+- 1 portion de fromage fondu
+- 2 oeufs
+- Quelques feuilles de salade
 
 ## Préparation
 ### Étape 1
 Laver les tomates et le concombre, puis les couper en tranches avec une mandoline.
+Mettre les oeufs à bouillir dans de l'eau pour une dizaine de minutes afin d'obtenir des oeufs durs.
 ### Étape 2
-Écraser l'avocat avec une fourchette, le saler et le poivrer.
+Écraser l'avocat avec une fourchette, le saler et le poivrer. Incorporer la portion de fromage à cette purée.
 ### Étape 3
-Couper la baguette en deux dans la longueur. Les tartiner avec la purée d'avocat.
+Couper la baguette en deux dans la longueur. Les tartiner avec la purée avocat/fromage.
+Découper les oeufs en tranches de l'épaisseur de votre choix.
 ### Étape 4
-Disposer sur la purée d'avocat les tranches de légumes et de fromage.
+Disposer sur la purée d'avocat les tranches de légumes et de fromage. Ajouter les tranches d'oeuf. Finir garnir avec des feuilles de salade.
 ### Étape 5
 Déguster!");
 
         $postSandwich1->setPublished(new \DateTime('NOW'));
         $manager->persist($postSandwich1);
+
+        //Recette de sandwich numéro 1
+        $postSandwich2 = new Post();
+        $postSandwich2->setTitle("Sandwich ");
+        $postSandwich2->setUrlAliasSlugified($postSandwich2->getTitle());
+        $postSandwich2->setContent("
+
+![](http://www.shelikesfood.com/wp-content/uploads/2015/12/Curried-Chickpea-Salad-Sandwiches-9249.jpg)
+
+## Ingrédients
+- 2 tranches de pain de campagne
+- 1 boîte de pois-chiches *égouttés et rincés*
+- 2 cuillères à soupe de mayonnaise
+- 2 cuillères à café de curry
+- 1 cuillère à café de moutarde
+- 1/2 oignon émincé **très finement**
+- De la roquette *optionnel*
+
+## Préparation
+### Étape 1
+Préparer la purée de pois-chiches en mettant dans un mixer les pois-chiches, la mayonnaise, la moutarde, le curry et les oignons
+### Étape 2
+Faire griller le pain. Disposer sur la première tranche les feuilles de roquette.
+### Étape 3
+Étaler la purée de pois-chiches sur la salade.
+### Étape 4
+Déguster!");
+
+        $postSandwich2->setPublished(new \DateTime('NOW'));
+        $manager->persist($postSandwich2);
+
 
         for ($i = 1; $i < 15; $i++)
         {
