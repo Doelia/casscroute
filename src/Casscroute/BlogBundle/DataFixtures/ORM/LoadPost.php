ef93ea8e5d21ddc75c287ec15dde75001da1c1b1
cavalet.pierre@gmail.com <pierre.cavalet@etu.umontpellier.fr>
2015-12-04 10:26:03 +0100

    gestion markdown + assetic (partie 1)

--- src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
+++ src/Casscroute/BlogBundle/DataFixtures/ORM/LoadPost.php
@@ -16,7 +16,11 @@ class LoadPost implements FixtureInterface
             $post = new Post();
             $post->setTitle("Article " . $i);
             $post->setUrlAliasSlugified($post->getTitle());
-            $post->setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta velit faucibus, laoreet nibh sit amet, molestie dui. Nulla eu ante lectus. Ut tempus, felis a accumsan mattis, lectus mi lacinia diam, malesuada dictum mi ipsum et mi. Nulla cursus sem tellus, eu tincidunt mauris ornare eu. Mauris ut dui viverra mauris ultrices lacinia. Mauris non ante eleifend, scelerisque nulla vitae, aliquet ligula. Proin dui risus, tristique at tortor ultrices, maximus suscipit magna. Nullam felis magna, laoreet ac mattis sed, feugiat eu mi. Donec ut consectetur sapien, a interdum ipsum. Duis consequat enim non tellus laoreet ornare. Nullam et tristique risus, iaculis semper nibh.");
+            $post->setContent("# Lorem ipsum
+### dolor sit amet\n
+consectetur adipiscing elit.\n
+### Mauris porta velit faucibus\n
+laoreet nibh sit amet, molestie dui. Nulla eu ante lectus. Ut tempus, felis a accumsan mattis, lectus mi lacinia diam, malesuada dictum mi ipsum et mi. Nulla cursus sem tellus, eu tincidunt mauris ornare eu. Mauris ut dui viverra mauris ultrices lacinia. Mauris non ante eleifend, scelerisque nulla vitae, aliquet ligula. Proin dui risus, tristique at tortor ultrices, maximus suscipit magna. Nullam felis magna, laoreet ac mattis sed, feugiat eu mi. Donec ut consectetur sapien, a interdum ipsum. Duis consequat enim non tellus laoreet ornare. Nullam et tristique risus, iaculis semper nibh.");
             $post->setPublished(new \DateTime("2015-01-01 10:00:" . $i));
             $manager->persist($post);
         }
